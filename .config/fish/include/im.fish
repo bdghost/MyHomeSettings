1122e439bddd2af81b6cfe410b7c9dadf9058433
iven <ivenvd@gmail.com>
2012-09-24 23:54:10 +0800

    General commit.

--- .config/fish/include/im.fish
+++ .config/fish/include/im.fish
@@ -1,8 +1,8 @@
 
-set -x XMODIFIERS @im=fcitx
-#set -x QT_IM_MODULE xim
-#set -x GTK_IM_MODULE xim
-set -x QT_IM_MODULE fcitx
-set -x GTK_IM_MODULE fcitx
+set -gx XMODIFIERS @im=fcitx
+set -gx XIM fcitx
+set -gx XIM_PROGRAM fcitx
+set -gx QT_IM_MODULE fcitx
+set -gx GTK_IM_MODULE fcitx
 
 # vim:ts=4:sw=4:et:ft=fish:
--- .config/fish/include/userpath.fish
+++
@@ -1,9 +0,0 @@
-
-for p in $HOME/Dropbox/Scripts $HOME/.gem/ruby/1.9.1/bin
-    if not [ -d $p ] continue; end
-    if not contains $p $PATH
-        set PATH $p $PATH
-    end
-end
-
-# vim:ts=4:sw=4:et:ft=fish:
