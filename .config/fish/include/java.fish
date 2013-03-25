e88b84630672234e4497e708fe54973f4ebe077c
Xu Lijian <xulijian@qiyi.com>
2013-03-25 14:48:16 +0800

    General commit.

---
+++ .config/fish/include/java.fish
@@ -0,0 +1,1 @@
+set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
--- .config/fish/initenv.fish
+++ .config/fish/initenv.fish
@@ -1,6 +1,6 @@
 
 # Set system path
-set -x PATH /usr/lib/ccache/bin $HOME/Scripts $HOME/.gem/ruby/1.9.1/bin /bin /usr/bin /sbin /usr/sbin /usr/bin/vendor_perl /usr/bin/core_perl
+set -x PATH /usr/lib/ccache/bin $HOME/Scripts $HOME/.gem/ruby/2.0.0/bin /bin /usr/bin /sbin /usr/sbin /usr/bin/vendor_perl /usr/bin/core_perl
 
 set -x CDPATH . ~
 
