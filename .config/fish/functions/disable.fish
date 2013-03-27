43c36b371fe6d238feea4c0eac91523ae7e670cc
iven <ivenvd@gmail.com>
2013-03-28 01:13:08 +0800

    General commit.

---
+++ .config/fish/functions/disable.fish
@@ -0,0 +1,7 @@
+
+function disable
+    sudo systemctl disable $argv
+end
+
+# vim:ts=4:sw=4:et:ft=fish:
+
--- .config/fish/include/im.fish
+++ .config/fish/include/im.fish
@@ -1,7 +1,7 @@
 
 set -gx XMODIFIERS @im=fcitx
-set -gx XIM fcitx
-set -gx XIM_PROGRAM fcitx
+#set -gx XIM fcitx
+#set -gx XIM_PROGRAM fcitx
 set -gx QT_IM_MODULE fcitx
 set -gx GTK_IM_MODULE fcitx
 
